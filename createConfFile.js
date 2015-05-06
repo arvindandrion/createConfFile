d17cfc2be25d494d47c99df534da5e7900726bea
Arvind Andrion <arvind@Arvinds-MacBook-Pro.local>
2015-05-05 22:30:19 -0600

    all functions are now working

--- createConfFile.js
+++ createConfFile.js
@@ -1,23 +1,62 @@
 var fs = require('fs');
+var chalk = require('chalk');
 var args = process.argv;
 
-// Check filename if has .conf file
-flname = args[2];
-flname = flname.split(".");
-
-if(flname[1] === "conf"){
-  console.log("yes it has .conf extension continue!");
+//*****************************************************
+// #1 Check if has validation
+//*****************************************************
+if(args[2] && args[3]){
+	console.log(chalk.green("nice work! Its two arguments"));
+	checkConfFile(args[2]); // if all is good go to #2
 }else{
-  console.log("Error! Please enter .conf extension!")
+	console.log(chalk.red("im expecting two arguments ex: name.conf, domainName"));
+	return false;
 }
 
+//*****************************************************
+// #2 Check if first arguement has conf file
+// The passed var args2 is args[2] in line 2 whichi is filename
+//*****************************************************
+function checkConfFile() {
+	flname = args[2];
+	flname = flname.split(".");
+	if(flname[1] === "conf"){
+	  console.log(chalk.green("Yes! It has .conf extension continue!"));
+	  checkFilenameExist(args[2]); // all good go to #3
+	}else{
+	  console.log(chalk.red("Error! Please enter .conf extension!"));
+	  return false;
+	}
+}
 
-// // check if the file exist
-// fs.stat(flname, function (err){
-//   if (err){
-//     console.log("file doesn't exist create file");
+//*****************************************************
+// #3 Check if filename exist
+// The passed var args2 is args[2] in line 2 whichi is filename
+//*****************************************************
+function checkFilenameExist() {
+	fs.stat(args[2], function (err){
+	  if (err){
+	    console.log(chalk.green("file doesn't exist create file"));
+	    createFile(args[2]);// all good go to #4
+	  }else{
+	    console.log(chalk.red("File exists, change filename"));
+	   	return false;
+	  } 
+	});
+}
 
-//   }else{
-//     console.log("File exists, change filename");
-//   } 
-// });
+//*****************************************************
+// #4 Create File
+// The passed var args2 is args[2] in line 2 whichi is filename
+//*****************************************************
+function createFile() {
+	var content = "content\n" + 
+	"content2";
+	fs.writeFile(args[2], content, function (err) {
+	  if (!err) {
+	  	console.log(chalk.green("File created! Congrats Jarvin!!!!!"));
+	  }else{
+	  	console.log(chalk.red("Can't write files"));
+	  }
+	});
+}
